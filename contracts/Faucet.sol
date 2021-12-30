45fb20bcb172d0b29bed887fa73d6a17472d31e6
"Steven Duncan" <"s.duncan@northeastern.edu">
2021-12-30 17:44:39 -0500

    new commit

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -12,7 +12,6 @@ contract Faucet {
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
         require(withdraw_amount <= 100000000000000000);
-
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
     }
