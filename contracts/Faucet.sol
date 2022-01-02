76c1cd8fa53905638c82669798bf3a7065dcad52
"Steven Duncan" <"s.duncan@northeastern.edu">
2022-01-02 11:51:26 -0500

    latest updates

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,19 +1,53 @@
 // SPDX-License-Identifier: CC-BY-SA-4.0
 
 // Version of Solidity compiler this program was written for
-pragma solidity 0.6.4;
+pragma solidity ^0.8.11;
 
-// Our first contract is a faucet!
-contract Faucet {
+contract Owned {
+    address payable owner;
+
+    // Contract constructor: set owner
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+}
+
+contract Mortal is Owned {
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
+
+contract Faucet is Mortal {
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
+
     // Accept any incoming amount
-    receive() external payable {}
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
-        require(withdraw_amount <= 100000000000000000);
+        assert(withdraw_amount <= 0.1 ether);
+
+        assert(
+            address(this).balance >= withdraw_amount
+        );
+
+         emit Withdrawal(msg.sender, withdraw_amount);
+
         // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
+
+       
     }
-    
 }
